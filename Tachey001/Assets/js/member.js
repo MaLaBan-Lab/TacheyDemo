3764cf830acfab49f89527565c97f474546a1b11
WIP_WU <0>
2021-06-10 22:45:50 +0800

    profile and setting update

--- Tachey001/Assets/js/member.js
+++ Tachey001/Assets/js/member.js
@@ -9,3 +9,25 @@ $(function () {
         return false;
     });
 });
+
+$("#user-pseudo-btn").on('click', function () {
+    $('.overlay-jobs').css("display", "block");
+});
+
+$("#user-interest-btn").on('click', function () {
+    $('.overlay-interest').css("display", "block");
+});
+
+$("#user-like-btn").on('click', function () {
+    $('.overlay-likes').css("display", "block");
+});
+
+$('.close-button').on('click', function () {
+    $('.overlay-jobs').css("display", "none");
+    $('.overlay-interest').css("display", "none");
+    $('.overlay-likes').css("display", "none");
+});
+
+$(".jobs-btn").on('click', function () {
+    $('.jobs-btn').toggleClass('toggle');
+});
---
+++ Tachey001/Assets/js/personal.js
@@ -0,0 +1,4 @@
+﻿$("#about-textarea-pen").on('click', function () {
+    $('#about-textarea').css("display", "block");
+    $('.about-content').css("display", "none");
+});
