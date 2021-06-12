d8d32d60860a0f8849a1b5e9f76202309b857291
George <nfd74108520@gmail.com>
2021-06-12 15:40:17 +0800

    更新 資料庫 會員類別 ID型別

--- Tachey001/Controllers/CoursesController.cs
+++ Tachey001/Controllers/CoursesController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using Tachey001.Models;
 
 namespace Tachey001.Controllers
 {
     [Authorize]
     public class CoursesController : Controller
     {
+        private TacheyContext tacheyDb = new TacheyContext();
+
         [AllowAnonymous]
         public ActionResult All()
         {
@@ -47,6 +50,22 @@ namespace Tachey001.Controllers
 
             ViewBag.Id = id;
             return View();
+        }
+
+
+        public ActionResult Step0()
+        {
+            var currentUserId = User.Identity.GetUserId();
+
+            Course newCourse = new Course { CourseID = 1, MemberID = currentUserId };
+
+            tacheyDb.Course.Add(newCourse);
+
+            tacheyDb.SaveChanges();
+
+            tacheyDb.Dispose();
+
+            return RedirectToAction("Step", "Courses", new { id = 1});
         }
     }
 }
