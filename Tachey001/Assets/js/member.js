fa083803b147134c0242a070ce9d132cf3497b82
WIP_WU <0>
2021-06-17 23:08:24 +0800

    帳號設定 工作多選 CRUD

--- Tachey001/Assets/js/member.js
+++ Tachey001/Assets/js/member.js
@@ -10,41 +10,9 @@ $(function () {
     });
 });
 
-$("#user-pseudo-btn").on('click', function () {
-    $('.overlay-jobs').css("display", "block");
-});
-
-$("#user-interest-btn").on('click', function () {
-    $('.overlay-interest').css("display", "block");
-});
-
-// 彈跳視窗-其他個人喜好
-$("#user-like-btn").on('click', function () {
-    $('.overlay-likes').css("display", "block");
-});
-
-// 關閉彈跳視窗
-$('.close-button').on('click', function () {
-    $('.overlay-jobs').css("display", "none");
-    $('.overlay-interest').css("display", "none");
-    $('.overlay-likes').css("display", "none");
-});
-
-$(".jobs-btn").on('click', function () {
-    let chk_btn_cou = document.querySelectorAll(".jobs-btn-checked");
-
-    // 按下工作選項
-    if ($(this).hasClass("jobs-btn")) {
-        $(this).addClass('jobs-btn-checked');
-        $(this).removeClass('jobs-btn');   
-    }
-    else {
-        $(this).removeClass('jobs-btn-checked');
-        $(this).addClass('jobs-btn');
-    }
-
+function check_job_btn_disable() {
     // 多於三個時，剩下選項不給選
-    chk_btn_cou = document.querySelectorAll(".jobs-btn-checked");
+    let chk_btn_cou = document.querySelectorAll(".jobs-btn-checked");
     if (chk_btn_cou.length > 2) {
         let btn_cou = document.querySelectorAll(".jobs-btn");
         btn_cou.forEach((item) => {
@@ -69,9 +37,82 @@ $(".jobs-btn").on('click', function () {
     else {
         $('.confirm-btn').attr('disabled', true);
     }
+}
+
+$("#user-pseudo-btn").on('click', function () {
+    $('.overlay-jobs').css("display", "block");
+    check_job_btn_disable();
 });
 
-$(".likes-btn").on('click', function () {
+$("#user-interest-btn").on('click', function () {
+    $('.overlay-interest').css("display", "block");
+});
+
+// 彈跳視窗-其他個人喜好
+$("#user-like-btn").on('click', function () {
+    $('.overlay-likes').css("display", "block");
+});
+
+// 關閉彈跳視窗
+$('.close-button').on('click', function () {
+    $('.overlay-jobs').css("display", "none");
+    $('.overlay-interest').css("display", "none");
+    $('.overlay-likes').css("display", "none");
+});
+
+$(".toggle-btn-job").on('click', function () {
+    // 按下工作選項
+    if ($(this).hasClass("jobs-btn")) {
+        $(this).addClass('jobs-btn-checked');
+        $(this).removeClass('jobs-btn');   
+    }
+    else {
+        $(this).removeClass('jobs-btn-checked');
+        $(this).addClass('jobs-btn');
+    }
+
+    check_job_btn_disable();
+    confirm_btn("SettingJob", "jobs");
+});
+
+function confirm_btn(url, blockname) {
+    let chk_btn = "";
+    // 使用jQuery ajax post 呼叫 .Net MVC Controller
+    (function ($) {
+        $.fn.toggleDisabled = function () {
+            return this.each(function () {
+                this.disabled = !this.disabled;
+            });
+        };
+    })(jQuery);
+
+    $('.confirm-btn').click(function () {
+
+        //失效按鈕 避免連點
+        $('.confirm-btn').attr('disabled', true);
+
+        let chk_btn_cou = document.querySelectorAll("." + blockname + "-btn-checked");
+
+        chk_btn_cou.forEach(function (val, index) {
+            chk_btn += val.innerText + "/";
+            console.log(val.innerText);
+        });
+
+        var actionUrl = url + "?" + "clickedOption" + "=" + chk_btn;
+
+        $.post(actionUrl, function (data) {
+            //console.log(data);
+            $('.confirm-btn').attr('disabled', true);
+            $('.overlay-' + blockname).css("display", "none");
+        })
+            .fail(function () {
+                console.log("something wrong...");
+            })
+    });
+}
+
+
+$(".toggle-btn-like").on('click', function () {
     let chk_btn_cou = document.querySelectorAll(".likes-btn-checked");
 
     // 按下喜好選項
@@ -92,6 +133,8 @@ $(".likes-btn").on('click', function () {
     else {
         $('.confirm-btn').attr('disabled', true);
     }
+
+    confirm_btn("SettingLike", "likes");
 });
 
 // input 監聽
@@ -102,44 +145,27 @@ $(".likes-btn").on('click', function () {
 //    }
 //});
 
-// select 監聽
-$('.bday-year').change(() => {
-    if ($('.bday-year').val() != '' && $('.bday-month').val() != '' && $('.bday-day').val() != '') {
-        $('.close-click-btn').prop('disabled', false);
+function close_click_btn_control() {
+    if ($('.bday-year').val() != '' && $('.bday-month').val() != '' && $('.bday-day').val() != '' && $('.form-check-inline').find('input[type="radio"]:checked').val() != undefined) {
+        $('#close-click-btn').prop('disabled', false);
     }
     else {
-        $('.close-click-btn').attr('disabled', true);
+        $('#close-click-btn').attr('disabled', true);
     }
+}
+// select 監聽
+$('.bday-year').change(() => {
+    close_click_btn_control();
 });
 
 $('.bday-month').change(() => {
-    if ($('.bday-year').val() != '' && $('.bday-month').val() != '' && $('.bday-day').val() != '') {
-        $('.close-click-btn').prop('disabled', false);
-    }
-    else {
-        $('.close-click-btn').attr('disabled', true);
-    }
+    close_click_btn_control();
 });
 
 $('.bday-day').change(() => {
-    if ($('.bday-year').val() != '' && $('.bday-month').val() != '' && $('.bday-day').val() != '') {
-        $('.close-click-btn').prop('disabled', false);
-    }
-    else {
-        $('.close-click-btn').attr('disabled', true);
-    }
+    close_click_btn_control();
 });
 
 $('.form-check-inline').change(() => {
-    //alert('123');
-    //var obj = document.getElementsByName("status_1");
-    //var selected = [];
-    //for (var i = 0; i < obj.length; i++) {
-    //    if (obj[i].checked) {
-    //        selected.push(obj[i].value);
-    //    }
-    //}
-    //alert("您勾選的項目 : " + selected.join());
-
-    //$("[name='status_1']:checked").val() 
+    close_click_btn_control();
 });
--- Tachey001/Controllers/MemberController.cs
+++ Tachey001/Controllers/MemberController.cs
@@ -118,54 +118,185 @@ namespace Tachey001.Controllers
                 ViewBag.emailStatus = result.EmailStatus;
                 ViewBag.sex = result.Sex;
 
-                if (result.Birthday != null)
+                if (result.Profession != null)
                 {
-                    DateTime birthday = DateTime.ParseExact(result.Birthday.ToString(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                    ViewBag.birthday = birthday;
+                    ViewBag.profession = result.Profession.Split('/');
                 }
                 else
                 {
-                    ViewBag.birthday = null;
+                    ViewBag.profession = result.Profession;
                 }
-
-                var selectList = new List<SelectListItem>()
+                var jobList = new List<string>()
                 {
-                    new SelectListItem {Text="text-1", Value="value-1" },
-                    new SelectListItem {Text="text-2", Value="value-2" },
-                    new SelectListItem {Text="text-3", Value="value-3" },
+                    "藝文設計", "出版業", "金融業", "製造業", "資訊科技", "營建工程", "科技業", "廣告傳播", "服務業", "家管", "自由業", "職業軍人", "公務人員", "教學專業", "法律、社會及文化專業", "醫療", "退休", "學生", "非營利組織", "其他"
+                };
+
+                ViewBag.jobList = jobList;
+
+                if (result.Like != null)
+                {
+                    ViewBag.like = result.Like.Split('/');
+                }
+                else
+                {
+                    ViewBag.like = result.Like;
+                }
+                var likeList = new List<string>()
+                {
+                    "旅行旅遊", "運動健身", "瑜珈", "桌遊", "棋類遊戲", "插花", "素描", "插畫", "水彩", "速寫", "手寫字", "書法", "電腦繪圖", "手作", "寫作", "社會服務", "電玩", "手遊", "電影", "電視劇", "舞台劇", "舞蹈", "閱讀", "狗派", "占卜", "美容妝髮", "區塊鏈", "金融理財", "運動賽事", "政治經濟"
+                };
+
+                ViewBag.likeList = likeList;
+
+
+                var selectListYear = new List<SelectListItem>()
+                {
+                    new SelectListItem {Text="2021", Value="2021" },
+                    new SelectListItem {Text="2020", Value="2020" },
+                    new SelectListItem {Text="2019", Value="2019" },
+                    new SelectListItem {Text="2018", Value="2018" },
+                    new SelectListItem {Text="2017", Value="2017" },
+                    new SelectListItem {Text="2016", Value="2016" },
+                    new SelectListItem {Text="2015", Value="2015" },
+                    new SelectListItem {Text="2014", Value="2014" },
+                    new SelectListItem {Text="2013", Value="2013" },
+                    new SelectListItem {Text="2012", Value="2012" },
+                    new SelectListItem {Text="2011", Value="2011" },
+                    new SelectListItem {Text="2010", Value="2010" },
+                    new SelectListItem {Text="2009", Value="2009" },
+                    new SelectListItem {Text="2008", Value="2008" },
+                    new SelectListItem {Text="2007", Value="2007" },
+                    new SelectListItem {Text="2006", Value="2006" },
+                    new SelectListItem {Text="2005", Value="2005" },
+                    new SelectListItem {Text="2004", Value="2004" },
+                    new SelectListItem {Text="2003", Value="2003" },
+                    new SelectListItem {Text="2002", Value="2002" },
+                    new SelectListItem {Text="2001", Value="2001" },
+                    new SelectListItem {Text="2000", Value="2000" },
+                    new SelectListItem {Text="1999", Value="1999" },
+                    new SelectListItem {Text="1998", Value="1998" },
+                    new SelectListItem {Text="1997", Value="1997" },
+                    new SelectListItem {Text="1996", Value="1996" },
+                    new SelectListItem {Text="1995", Value="1995" },
+                    new SelectListItem {Text="1994", Value="1994" },
+                    new SelectListItem {Text="1993", Value="1993" },
+                    new SelectListItem {Text="1992", Value="1992" },
+                    new SelectListItem {Text="1991", Value="1991" },
+                    new SelectListItem {Text="1990", Value="1990" },
+                    new SelectListItem {Text="1989", Value="1989" },
+                    new SelectListItem {Text="1988", Value="1988" },
+                    new SelectListItem {Text="1987", Value="1987" },
+                    new SelectListItem {Text="1986", Value="1986" },
+                    new SelectListItem {Text="1985", Value="1985" },
+                    new SelectListItem {Text="1984", Value="1984" },
+                    new SelectListItem {Text="1983", Value="1983" },
+                    new SelectListItem {Text="1982", Value="1982" },
+                    new SelectListItem {Text="1981", Value="1981" },
+                    new SelectListItem {Text="1980", Value="1980" },
+                    new SelectListItem {Text="1979", Value="1979" },
+                    new SelectListItem {Text="1978", Value="1978" },
+                    new SelectListItem {Text="1977", Value="1977" },
+                    new SelectListItem {Text="1976", Value="1976" },
+                    new SelectListItem {Text="1975", Value="1975" },
+                    new SelectListItem {Text="1974", Value="1974" },
+                    new SelectListItem {Text="1973", Value="1973" },
+                    new SelectListItem {Text="1972", Value="1972" },
+                    new SelectListItem {Text="1971", Value="1971" },
+                    new SelectListItem {Text="1970", Value="1970" },
+                };
+
+                var selectListMonth = new List<SelectListItem>()
+                {
+                    new SelectListItem {Text="1", Value="1" },
+                    new SelectListItem {Text="2", Value="2" },
+                    new SelectListItem {Text="3", Value="3" },
+                    new SelectListItem {Text="4", Value="4" },
+                    new SelectListItem {Text="5", Value="5" },
+                    new SelectListItem {Text="6", Value="6" },
+                    new SelectListItem {Text="7", Value="7" },
+                    new SelectListItem {Text="8", Value="8" },
+                    new SelectListItem {Text="9", Value="9" },
+                    new SelectListItem {Text="10", Value="10" },
+                    new SelectListItem {Text="11", Value="11" },
+                    new SelectListItem {Text="12", Value="12" },
+                };
+
+                var selectListDay = new List<SelectListItem>()
+                {
+                    new SelectListItem {Text="1", Value="1" },
+                    new SelectListItem {Text="2", Value="2" },
+                    new SelectListItem {Text="3", Value="3" },
+                    new SelectListItem {Text="4", Value="4" },
+                    new SelectListItem {Text="5", Value="5" },
+                    new SelectListItem {Text="6", Value="6" },
+                    new SelectListItem {Text="7", Value="7" },
+                    new SelectListItem {Text="8", Value="8" },
+                    new SelectListItem {Text="9", Value="9" },
+                    new SelectListItem {Text="10", Value="10" },
+                    new SelectListItem {Text="11", Value="11" },
+                    new SelectListItem {Text="12", Value="12" },
+                    new SelectListItem {Text="13", Value="13" },
+                    new SelectListItem {Text="14", Value="14" },
+                    new SelectListItem {Text="15", Value="15" },
+                    new SelectListItem {Text="16", Value="16" },
+                    new SelectListItem {Text="17", Value="17" },
+                    new SelectListItem {Text="18", Value="18" },
+                    new SelectListItem {Text="19", Value="19" },
+                    new SelectListItem {Text="20", Value="20" },
+                    new SelectListItem {Text="21", Value="21" },
+                    new SelectListItem {Text="22", Value="22" },
+                    new SelectListItem {Text="23", Value="23" },
+                    new SelectListItem {Text="24", Value="24" },
+                    new SelectListItem {Text="25", Value="25" },
+                    new SelectListItem {Text="26", Value="26" },
+                    new SelectListItem {Text="27", Value="27" },
+                    new SelectListItem {Text="28", Value="28" },
+                    new SelectListItem {Text="29", Value="29" },
+                    new SelectListItem {Text="30", Value="30" },
+                    new SelectListItem {Text="31", Value="31" },
                 };
 
                 //預設選擇哪一筆
-                selectList.Where(q => q.Value == "value-2").First().Selected = true;
+                //selectList.Where(q => q.Value == "男").First().Selected = true;
+                if (result.Birthday != null)
+                {
+                    DateTime date = DateTime.Parse(result.Birthday.ToString());
+                    selectListYear.Where(q => q.Value == date.ToString().Split('/')[0]).First().Selected = true;
+                    selectListMonth.Where(q => q.Value == date.ToString().Split('/')[1]).First().Selected = true;
+                    selectListDay.Where(q => q.Value == date.ToString().Split('/')[2].Split(' ')[0]).First().Selected = true;
+                }
 
-                ViewBag.SelectList = selectList;
+                
+
+                ViewBag.SelectListYear = selectListYear;
+                ViewBag.SelectListMonth = selectListMonth;
+                ViewBag.SelectListDay = selectListDay;
 
                 //context.SaveChanges();
             }
+
+            
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult SettingIndex()
+        public ActionResult SettingIndex(FormCollection frmcol, string Sex)
         {
-            //int personId = person.PersonId;
-            //string name = person.Name;
-            //string gender = person.Gender;
-            //string city = person.City;
+            var year = frmcol["year"]; //get value
+            var month = frmcol["month"]; //get value
+            var date = frmcol["date"]; //get value
 
             var UserId = User.Identity.GetUserId();
 
             var result = _context.Member.Find(UserId);
 
-            //result.Title = Title;
-            //result.Description = Description;
-            //result.TitlePageImageURL = TitlePageImageURL;
-            result.Sex = ViewBag.SelectList;
+            result.Birthday = Convert.ToDateTime(date + "/" + month + "/" + year);
+            result.Sex = Sex;
 
             _context.SaveChanges();
 
-            return View();
+            return RedirectToAction("Setting", "Member");
         }
 
         [HttpPost]
@@ -315,18 +446,74 @@ namespace Tachey001.Controllers
                 //result.Title = Title;
                 //result.Description = Description;
                 //result.TitlePageImageURL = TitlePageImageURL;
-                result.Introduction = Introduction;
-                result.Introduction = Introduction;
-                result.Introduction = Introduction;
-                result.Introduction = Introduction;
-                result.Introduction = Introduction;
-                result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
 
                 _context.SaveChanges();
 
             }
 
             return RedirectToAction("Profile", "Member");
+        }
+
+        [HttpPost]
+        public ActionResult SettingJob(string clickedOption)
+        {
+            
+                //int personId = person.PersonId;
+                //string name = person.Name;
+                //string gender = person.Gender;
+                //string city = person.City;
+
+                var UserId = User.Identity.GetUserId();
+
+                var result = _context.Member.Find(UserId); // not member
+
+                result.Profession = clickedOption;
+                //result.Description = Description;
+                //result.TitlePageImageURL = TitlePageImageURL;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+                //result.Introduction = Introduction;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Setting", "Member");
+        }
+
+        [HttpPost]
+        public ActionResult SettingLike(string clickedOption)
+        {
+
+            //int personId = person.PersonId;
+            //string name = person.Name;
+            //string gender = person.Gender;
+            //string city = person.City;
+
+            var UserId = User.Identity.GetUserId();
+
+            var result = _context.Member.Find(UserId); // not member
+
+            result.Like = clickedOption;
+            //result.Description = Description;
+            //result.TitlePageImageURL = TitlePageImageURL;
+            //result.Introduction = Introduction;
+            //result.Introduction = Introduction;
+            //result.Introduction = Introduction;
+            //result.Introduction = Introduction;
+            //result.Introduction = Introduction;
+            //result.Introduction = Introduction;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Setting", "Member");
         }
 
         [HttpPost]
@@ -473,8 +660,8 @@ namespace Tachey001.Controllers
                 ViewBag.blogConnection = "";
             }
 
-            // 課程
-            ViewBag.courseGive = from p in _context.Course where p.MemberID == UserId select p; // 開課
+            // 課程var courseList = tacheyDb.Course.Where(x => x.MemberID == currentId).Select(x => x).ToList();
+            ViewBag.courseGive = (from p in _context.Course where p.MemberID == UserId select p).ToList(); // 開課
             ViewBag.courseTake = from p in _context.CourseBuyed where p.MemberID == UserId select p; // 修課
             ViewBag.courseFavorites = from p in _context.Owner where p.MemberID == UserId select p; // 收藏
             ViewBag.courseWork = from p in _context.Homework where p.MemberID == UserId select p; // 作品
@@ -491,6 +678,22 @@ namespace Tachey001.Controllers
             //var courseList1 = tacheyDb.Course.Where(x => x.MemberID == currentId).Select(x => x).ToList();
 
             //return View(courseList, courseList1);
+
+            var personalUrl = from p in _context.PersonalUrl
+                              join m in _context.Member on p.MemberID equals m.MemberID
+                              where p.MemberID == UserId
+                              select new PersonalUrlView
+                              {
+                                  FbUrl = p.FbUrl,
+                                  GoogleUrl = p.GoogleUrl,
+                                  YouTubeUrl = p.YouTubeUrl,
+                                  BehanceUrl = p.BehanceUrl,
+                                  BlogUrl = p.BlogUrl,
+                                  PinterestUrl = p.PinterestUrl,
+                                  Name = m.Name
+                              };
+
+            ViewBag.UserPhoto = tacheyDb.Member.Find(UserId).Photo;
 
             return View();
         }
