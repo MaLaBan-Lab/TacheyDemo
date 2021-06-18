3509c02ac424ddc2f515e88f8da028425e2f4697
Emily <b10620002@g.chu.edu.tw>
2021-06-19 00:23:44 +0800

    Change home index bottom css Add ViewModel Card、Highlight、Comment、Card_highlightModel_group Add HomeService Add HomeRepository Change index type

---
+++ Tachey001/ViewModel/HighlightCourseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tachey001.ViewModel
+{
+    public class HighlightCourseViewModel
+    {
+        public string Title { get; set; }
+        public string Introduction { get; set; }
+        public int? TotalMinTime { get; set; }
+        public string Description { get; set; }
+    }
+}
