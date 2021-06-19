d10baafbdf73ba739ff9661f5d3369c3899699ad
b10620008 <b10620008@g.chu.edu.tw>
2021-06-19 17:35:42 +0800

    new consoleService,consoleRepository

---
+++ Tachey001/Repository/consoleRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tachey001.Models;
+
+namespace Tachey001.Repository
+{
+    public class consoleRepository
+    {
+        private TacheyContext _tacheyContext;
+
+        public consoleRepository()
+        {
+            _tacheyContext = new TacheyContext();
+        }
+
+        // Read 讀取課程表
+        public IQueryable<Models.Course> GetAllCourse()
+        {
+            var result = _tacheyContext.Course;
+
+            return result;
+        }
+        // Read 讀取會員表
+        public IQueryable<Member> GetAllMember()
+        {
+            var result = _tacheyContext.Member;
+
+            return result;
+        }
+        //取得指定課程資料表
+        public Models.Course GetCurrentCourse(string currentId)
+        {
+            var result = _tacheyContext.Course.Find(currentId);
+
+            return result;
+        }
+    }
+}
