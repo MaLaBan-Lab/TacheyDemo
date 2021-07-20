8df1ba674decb6966e2ac1da157dc16d6bd729cb
George <nfd74108520@gmail.com>
2021-07-20 11:24:31 +0800

    課程影片 新增+測試 評分，問題不跳轉

--- Tachey001/Assets/js/Main.js
+++ Tachey001/Assets/js/Main.js
@@ -31,12 +31,9 @@ window.onload = function () {
     });
 }
 
-
-
-//POST Step Fun
+//POST Score
 function postScore(CId) {
     var data = new FormData($(`#ScorePost`)[0]);
-    console.dir(data);
 
     $.ajax({
         type: "POST",
@@ -48,10 +45,76 @@ function postScore(CId) {
             if ($('#emptyScore') != undefined) {
                 $('#emptyScore').remove();
             }
-            $('#ScoreContainer').append(response)
+            $('#ScoreContainer').prepend(response)
             $('#scoreBtn').attr('disabled', true)
 
+            var val = parseInt($("input[name='PostCourseScore.Score']:checked").val());
+            var sco = parseInt($('#scoreAvg').get(0).textContent)
+            var avg = Math.ceil((sco + val) / 2)
+            $('#scoreAvg').get(0).textContent = avg
+            $('#scorePeople').get(0).textContent++
+
+            $('#scoreStar').children().each((index, item) => {
+                if (index < avg) {
+                    item.innerHTML = `
+                        <i class="
+                                        fas
+                                        fa-star
+                                        star-yellow
+                                        position-absolute
+                                        star-display
+                                      "></i>
+                    `
+                }
+            })
+
             $('#exampleModal').modal('hide')
+        },
+        error: function (err) {
+            console.log(err.ErrMsg)
+        }
+    })
+}
+
+//POST Ques
+function postQues(CId) {
+    var data = new FormData($(`#QuesPost`)[0]);
+
+    $.ajax({
+        type: "POST",
+        url: `/Courses/CreateQuestion?CourseId=${CId}`,
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            if ($('#emptyQues') != undefined) {
+                $('#emptyQues').remove();
+            }
+            $('#QuesContainer').append(response)
+
+            $('#QuesModal').modal('hide')
+        },
+        error: function (err) {
+            console.log(err.ErrMsg)
+        }
+    })
+}
+
+//POST Ans
+function postAns(CId, QId) {
+    var data = new FormData($(`#AnsPost-${QId}`)[0]);
+
+    $.ajax({
+        type: "POST",
+        url: `/Courses/CreateAnswer?CourseId=${CId}&QuestionId=${QId}`,
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            $(`#AnsContainer-${QId}`).append(response)
+
+            $(`#ques-${QId}-count`).get(0).textContent++;
+
         },
         error: function (err) {
             console.log(err.ErrMsg)
