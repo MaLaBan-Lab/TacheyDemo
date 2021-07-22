78848fd9af9e05c7164bd2cdfcb3dfc39b0b43a7
George <nfd74108520@gmail.com>
2021-07-22 22:25:33 +0800

    新增上傳課程主要影片

--- Tachey001/Service/CourseService.cs
+++ Tachey001/Service/CourseService.cs
@@ -504,14 +504,29 @@ namespace Tachey001.Service
                 PublicId = "PreviewVideo",
                 Folder = $"Course/{CourseId}"
             };
-
-            var f = new StreamReader(file.InputStream).ReadToEnd();
             
             var CallBackUrl = _cloudinary.UploadLarge(uploadParams).SecureUrl.ToString();
-            //Functions.SendProgress("影片上傳中 ... ", , file.ContentLength);
 
             var result = _tacheyRepository.Get<Course>(x => x.CourseID == CourseId);
             result.PreviewVideo = CallBackUrl;
+            _tacheyRepository.SaveChanges();
+
+            return CallBackUrl;
+        }
+        //儲存雲端上傳課程影片，並回傳網址
+        public string PostCourseVideoStorage(string CourseId, string UnitId, HttpPostedFileBase file)
+        {
+            var uploadParams = new VideoUploadParams()
+            {
+                File = new FileDescription("PreviewVideo", file.InputStream),
+                PublicId = UnitId,
+                Folder = $"Course/{CourseId}/Video"
+            };
+
+            var CallBackUrl = _cloudinary.UploadLarge(uploadParams).SecureUrl.ToString();
+
+            var result = _tacheyRepository.Get<CourseUnit>(x => x.CourseID == CourseId && x.UnitID == UnitId);
+            result.CourseURL = CallBackUrl;
             _tacheyRepository.SaveChanges();
 
             return CallBackUrl;
