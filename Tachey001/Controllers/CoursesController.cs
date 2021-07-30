ab2bf7f59692a16a270f3cd82e1249bec7a8c26b
George <nfd74108520@gmail.com>
2021-07-30 10:54:39 +0800

    修正

--- Tachey001/Controllers/CoursesController.cs
+++ Tachey001/Controllers/CoursesController.cs
@@ -15,7 +15,7 @@ using Tachey001.ViewModel.ApiViewModel;
 
 namespace Tachey001.Controllers
 {
-    [System.Web.Http.Authorize]
+    [Authorize]
     public class CoursesController : Controller
     {
         private TacheyContext tacheyDb;
